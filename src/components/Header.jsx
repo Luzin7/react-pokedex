3673eaab9f1612c8e12b63d7ce75db6bf061f7d9
Luan Victor <lvictor.dutra@gmail.com>
2022-05-05 21:49:48 -0300

    ♻️refactor: melhora estrutura
    Agora o nome dos pokémons e usuário possuem a primeira letra em caixa alta, para melhor visualização.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,22 @@ import "../style/header.css";
 export default class Header extends Component {
   render() {
     const { userName } = this.props;
+    const caps = (str) => {
+      return str.charAt(0).toUpperCase() + str.substr(1);
+    };
     return (
       <header>
         <section className="header">
           <div className="content__header">
-            <h1 className="title title__header">Olá, <span>{userName}!</span></h1>
+            <h1 className="title__header">
+              Olá, <span>{caps(userName)}!</span>
+            </h1>
             <p>
-              Esta é a sua <span>Pokedéx</span>!
+              Esta é a sua{" "}
+              <span className="subtitle__header">
+                <a href="#maine">Pokedéx</a>
+              </span>
+              !
             </p>
           </div>
         </section>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-
 import "../style/components/pokemon-card/card-Box.css";
+import "../style/components/pokemon-card/card-Content.css";
 
 export default class PokemonCard extends Component {
   render() {
-    const { name, type, img } = this.props;
+    const { order, name, type, img } = this.props;
+    const caps = (str) => {
+      return str.charAt(0).toUpperCase() + str.substr(1);
+    };
     return (
       <div className="card__box">
-        <span className="pokemon__img">
-          <img src={img} alt={name} />
-        </span>
-        <h1 className="title name__pokemon">{`Este(a) é ${name}`}</h1>
-        <h2 className="title type__pokemon">{`Seu tipo é ${type}`}</h2>
+        <div className="pokemon__img__box">
+          <img className="pokemon__img" src={img} alt={name} />
+        </div>
+        <div className="card__content">
+          <div className="pokemon__order">
+            <span className="order">{`#${order}`}</span>
+          </div>
+          <h1 className="title name__pokemon">{`Este(a) é ${caps(name)}`}</h1>
+          <h2 className="type__pokemon">{`Seu tipo é ${caps(type)}`}</h2>
+        </div>
       </div>
     );
   }
